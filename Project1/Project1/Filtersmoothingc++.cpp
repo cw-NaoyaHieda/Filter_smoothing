816e2294d59d243bd9e7311bed35d755c1579d07
naoya1218 <hero12185@gmail.com>
2017-12-16 05:08:05 +0900

    beta0.65

--- Project1/Project1/Filtersmoothingc++.cpp
+++ Project1/Project1/Filtersmoothingc++.cpp
@@ -10,7 +10,7 @@
 #include "lbfgs.h"
 #define GNUPLOT_PATH "C:/PROGRA~2/gnuplot/bin/gnuplot.exe"
 #define M_PI 3.14159265359
-#define beta 0.85
+#define beta 0.65
 #define q_qnorm -2.053749 //q‚É’¼‚µ‚½‚Æ‚«‚ÉA–ñ0.02
 #define rho 0.08
 #define X_0 -2.5
